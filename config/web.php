614598d45c1f81aa42c441a7e223267b127052b9
Никита <mrnkravchenko@gmail.com>
2018-06-27 21:00:59 +0300

    Create new project with yii2 framework

--- config/web.php
+++ config/web.php
@@ -43,14 +43,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<action>'=>'site/<action>',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
