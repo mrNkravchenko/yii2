60665d45119e7e932b577bddeef6a2b0880f99a5
Никита <mrnkravchenko@gmail.com>
2018-06-28 21:24:17 +0300

    Lesson3 correct code style and use URL helper

--- models/User.php
+++ models/User.php
@@ -49,129 +49,134 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
     {
         return [
             'id' => 'ID',
-            'username' => 'Username',
-            'surname' => 'Surname',
-            'name' => 'Name',
-            'password' => 'Password',
+            'username' => 'Ваш логин / email адрес',
+            'surname' => 'Фамилия',
+            'name' => 'Имя',
+            'password' => 'Пароль',
             'salt' => 'Salt',
             'access_token' => 'Access Token',
             'create_date' => 'Create Date',
         ];
     }
+
     /**
      * Before save event handler
      * @param bool $insert
      * @return bool
      */
-    public function beforeSave ($insert)
+    public function beforeSave($insert)
     {
-        if (parent::beforeSave($insert))
-        {
-            if ($this->getIsNewRecord() && !empty($this->password))
-            {
+        if (parent::beforeSave($insert)) {
+            if ($this->getIsNewRecord() && !empty($this->password)) {
                 $this->salt = $this->saltGenerator();
             }
-            if (!empty($this->password))
-            {
+            if (!empty($this->password)) {
                 $this->password = $this->passWithSalt($this->password, $this->salt);
-            }
-            else
-            {
+            } else {
                 unset($this->password);
             }
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
+
     /**
      * Generate the salt
      * @return string
      */
-    public function saltGenerator ()
+    public function saltGenerator()
     {
         return hash("sha512", uniqid('salt_', true));
     }
+
     /**
      * Return pass with the salt
      * @param $password
      * @param $salt
      * @return string
      */
-    public function passWithSalt ($password, $salt)
+    public function passWithSalt($password, $salt)
     {
         return hash("sha512", $password . $salt);
     }
+
     /**
      * @inheritdoc
      */
-    public static function findIdentity ($id)
+    public static function findIdentity($id)
     {
         return static::findOne(['id' => $id]);
     }
+
     /**
      * @inheritdoc
      */
-    public static function findIdentityByAccessToken ($token, $type = null)
+    public static function findIdentityByAccessToken($token, $type = null)
     {
         return static::findOne(['access_token' => $token]);
     }
+
     /**
      * Finds user by username
      *
      * @param string $username
      * @return static|null
      */
-    public static function findByUsername ($username)
+    public static function findByUsername($username)
     {
         return static::findOne(['username' => $username]);
     }
+
     /**
      * @inheritdoc
      */
-    public function getId ()
+    public function getId()
     {
         return $this->id;
     }
+
     /**
      * @inheritdoc
      */
-    public function getAuthKey ()
+    public function getAuthKey()
     {
         return $this->access_token;
     }
+
     /**
      * @inheritdoc
      */
-    public function validateAuthKey ($authKey)
+    public function validateAuthKey($authKey)
     {
         return $this->getAuthKey() === $authKey;
     }
+
     /**
      * Validates password
      *
      * @param  string $password password to validate
      * @return boolean if password provided is valid for current user
      */
-    public function validatePassword ($password)
+    public function validatePassword($password)
     {
         return $this->password === $this->passWithSalt($password, $this->salt);
     }
+
     /**
      * Generates password hash from password and sets it to the model
      *
      * @param string $password
      */
-    public function setPassword ($password)
+    public function setPassword($password)
     {
         $this->password = $this->passWithSalt($password, $this->saltGenerator());
     }
+
     /**
      * Generates "remember me" authentication key
      */
-    public function generateAuthKey ()
+    public function generateAuthKey()
     {
         $this->access_token = Yii::$app->security->generateRandomString();
     }
--- views/site/login.php
+++ views/site/login.php
@@ -40,14 +40,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <a href="/user/create" class="btn btn-primary">Sign up</a>
+                <a href="<?=\yii\helpers\Url::to(['/user/create'])?>" class="btn btn-primary">Sign up</a>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
+
 </div>
